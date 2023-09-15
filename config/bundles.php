5448de88ec594bc0f12453f3a614624e6755fbfd
fbernet <florent.bernet@outlook.com>
2023-09-15 12:55:19 +0200

    add webpack + bootstrap

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
