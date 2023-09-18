464b4c57cadc861fe18eb57d6f84e13f0324d408
fbernet <florent.bernet@outlook.com>
2023-09-18 17:13:38 +0200

    add css reset + additional css

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,6 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
+import './styles/reset.scss';
 import 'bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input';
